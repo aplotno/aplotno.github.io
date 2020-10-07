ee7a1c2a0d8253b53ce18bee9ca21721f1c37284
aplotno <64718638+aplotno@users.noreply.github.com>
2020-10-06 22:17:07 -0300

    Nueva version

--- js/init.js
+++ js/init.js
@@ -3,6 +3,8 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+    $('.modal').modal();
+    $('.collapsible').collapsible();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
